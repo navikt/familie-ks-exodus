a3304153fb10d7a1425c9b746853461857939678
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2026-09-09 14:27:29 +0200

    oppdater init.sh

--- init-scripts/init.sh
+++ init-scripts/init.sh
@@ -1,5 +1,4 @@
-export APP_DATASOURCE_URL=$(cat /secrets/oracle/config/jdbc_url)
-export APP_DATASOURCE_USERNAME=$(cat /secrets/oracle/credentials/username)
-export APP_DATASOURCE_PASSWORD=$(cat /secrets/oracle/credentials/password)
+export APP_DATASOURCE_USERNAME=$(cat "$APP_DATASOURCE_USERNAME_PATH" 2> /dev/null || echo $APP_DATASOURCE_USERNAME)
+export APP_DATASOURCE_PASSWORD=$(cat "$APP_DATASOURCE_PASSWORD_PATH" 2> /dev/null || echo $APP_DATASOURCE_PASSWORD)
 
 exec java -jar app.jar
